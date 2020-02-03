1cf89aa57246d070750d23b0862f05eedfaef1ee
Siddheswar Mukherjee <siddhu06ran@gmail.com>
2020-02-03 14:25:30 +0100

    Update db_config.py

--- content_recommendation/db_config.py
+++ content_recommendation/db_config.py
@@ -2,14 +2,19 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon 19 Aug 2019 16∶51∶45 2019
-
 @author: smkj33
+
+NOTE: This is the database configuration file.
+Please modify accordingly in order to gain access to database instance.
+    If db is running on simple local host, then just change the user and passwd.
+    If connection requires SSH protocol then modify config file accordingly.
+
 """
 
 connection_properties = {
     'host': '127.0.0.1',
     'port': 3306,
     'user': 'root',
-    'passwd': 'log',
+    'passwd': '<insert own cred.>',
     'db': 'db426841_843'
     }
